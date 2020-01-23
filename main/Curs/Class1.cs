8f9bfed5aad76a5af49adc96b5e9d408931bed45
ArtSerov <slonlosos.iban@gmail.com>
2020-01-23 14:32:53 +0300

    add save

--- main/Curs/Class1.cs
+++ main/Curs/Class1.cs
@@ -1,31 +1,38 @@
-﻿using System;
+﻿using Curs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Coursework
+namespace Cours
 
 {
-   
-    public class FamilyOfAnimals
+
+    public class FamilyOfAnimals: IWritableObject
     {
         private string family_name;
         private int Population;
 
-        public FamilyOfAnimals() {}
+        public FamilyOfAnimals() { }
         public FamilyOfAnimals(string fam_name)
         {
             family_name = fam_name;
         }
         public int getpop { get { return Population; } }
-        public string getfamily_name { get{return family_name; } }
-        public void SetFN (string name){ family_name = name; }
+        public string getfamily_name { get { return family_name; } }
+        public void SetFN(string name) { family_name = name; }
         public void SetPop(int pop) { Population += pop; }
+
+        public void Write(SaveManeger man)
+        {
+            man.WriteLine($"family name: {family_name}");
+            man.WriteLine($"population: {Population}");
+        }
     }
 
-    public class TypeOfAnimal
+    public class TypeOfAnimal: IWritableObject
     {
         private readonly string Name;
         private readonly float daily_feed_intake;
@@ -56,20 +63,25 @@ namespace Coursework
         public string GetName { get { return Name; } }
         public float GetDFI { get { return daily_feed_intake; } }
         public string GetContinent { get { return continent_habitat; } }
-        public bool GetReservoir {get{ return Reservoir; } }
+        public bool GetReservoir { get { return Reservoir; } }
         public bool GetHeat { get { return Heat; } }
         public void SetReservoir(bool x) { Reservoir = x; }
         public void SetHeat(bool x) { Heat = x; }
 
         public override string ToString()
         {
-            return Name + " " +" из семейства "+ family.getfamily_name;
+            return Name + " " + " из семейства " + family.getfamily_name;
         }
 
+        public void Write(SaveManeger man)
+        {
+            man.WriteLine($"Name: {Name}");
+            man.WriteLine($"Continent habitat: {continent_habitat}");
+        }
     }
-    public class Complex
+    public class Complex: IWritableObject
     {
-        
+
         private readonly string name_complex;
         private readonly int room_number;
         private bool availability_water;
@@ -99,8 +111,8 @@ namespace Coursework
         }
 
         public void adding(TypeOfAnimal animals)
-        {     
-                presents.Add(animals, 0);   
+        {
+            presents.Add(animals, 0);
         }
         public void adding(List<TypeOfAnimal> animals)
         {
@@ -110,11 +122,11 @@ namespace Coursework
             for (int i = 0; i < animals.Count; i++)
             {
                 if (animals[i].GetHeat == presence_heat && animals[i].GetReservoir == availability_water)
-                { 
+                {
                     Console.WriteLine(animals[i].GetName);
                     flag = true;
                 }
-                
+
             }
             if (flag == false)
             {
@@ -148,19 +160,19 @@ namespace Coursework
                 {
                     Console.WriteLine("Животное этого вида уже может проживать в этом комплексе");
                     return;
-                    
+
                 }
             }
         }
-        public void AddAni(string typeofani,int kol_vo, ref bool flag)
+        public void AddAni(string typeofani, int kol_vo, ref bool flag)
         {
             for (int i = 0; i < presents.Count; i++)
             {
                 var key = presents.ElementAt(i).Key;
-                if (key.GetName==typeofani)
+                if (key.GetName == typeofani)
                 {
                     presents[key] += kol_vo;
-                    daily_feed += key.GetDFI*kol_vo;
+                    daily_feed += key.GetDFI * kol_vo;
                     animals_in_room += kol_vo;
                     key.SetPop(kol_vo);
                     Console.WriteLine($"Животное из семейства {key.GetName} добавленно в {name_complex} комплекс в количестве {kol_vo}");
@@ -168,7 +180,7 @@ namespace Coursework
                 }
             }
         }
-        public void DelAni(string typeofani,int kol_vo)
+        public void DelAni(string typeofani, int kol_vo)
         {
             for (int i = 0; i < presents.Count; i++)
             {
@@ -180,10 +192,10 @@ namespace Coursework
                     animals_in_room -= kol_vo;
                     key.SetPop(-kol_vo);
                     Console.WriteLine($"{kol_vo} животное из семейства {key.GetName} выселено из комплекса {name_complex}");
-                   
+
                 }
             }
-        } 
+        }
         public void Water(string water)
         {
             if (water == "есть водоем")
@@ -192,37 +204,42 @@ namespace Coursework
         }
         public void Heat(string heat)
         {
-            if(heat == "есть отопление")
+            if (heat == "есть отопление")
                 presence_heat = true;
             else presence_heat = false;
         }
-        public bool GetWater {get{ return availability_water; } }
+        public bool GetWater { get { return availability_water; } }
         public bool GetHeat { get { return presence_heat; } }
         public override string ToString()
         {
-            return "комплекс "+name_complex+" "+room_number;
+            return "комплекс " + name_complex + " " + room_number;
         }
         public void WriteData()
         {
-            
-            Console.WriteLine($"\n В комплексе {name_complex+" "+room_number} находиться");
+
+            Console.WriteLine($"\n В комплексе {name_complex + " " + room_number} находиться");
             for (int i = 0; i < presents.Count; i++)
             {
-                if (presents.ElementAt(i).Value>0)
+                if (presents.ElementAt(i).Value > 0)
                     Console.WriteLine($"{presents.ElementAt(i).Key.ToString()} в количестве { presents.ElementAt(i).Value} ");
             }
         }
 
+        public void Write(SaveManeger man)
+        {
+            man.WriteLine($"Name complex: { name_complex}");
+            man.WriteLine($"room number: {room_number}");
+        }
     }
 
     public class Resettlement
     {
-       
+
         public static void AddInComplex(List<Complex> complices, List<TypeOfAnimal> animals)
         {
-            for (int i = 0; i <animals.Count; i++)
+            for (int i = 0; i < animals.Count; i++)
             {
-                for (int j = 0; j <complices.Count; j++)
+                for (int j = 0; j < complices.Count; j++)
                 {
                     if (complices[j].GetWater == true && animals[i].GetReservoir == true && complices[j].GetHeat == false && animals[i].GetHeat == false)
                     {
@@ -244,24 +261,24 @@ namespace Coursework
                     {
                         Console.WriteLine($"{animals[i].ToString()}\t  могут быть помешенны помещены в  {complices[j].ToString()}.");
                         complices[j].adding(animals[i]);
-                    } 
+                    }
                 }
-                
+
             }
-            
+
         }
 
-        public static void AddAnimalsInComplex( List<Complex> complices, List<TypeOfAnimal> animals)
+        public static void AddAnimalsInComplex(List<Complex> complices, List<TypeOfAnimal> animals)
         {
             Console.WriteLine("******************************");
             Console.WriteLine("\nПоселите животного в комплекс");
             Console.WriteLine("Виды животных которые могут проживать в зоопарке");
             for (int i = 0; i < animals.Count; i++) Console.WriteLine(animals[i].GetName);
             Console.WriteLine("Введите вид животного которого хотите поселить");
-            string animal = Console.ReadLine();        
+            string animal = Console.ReadLine();
             Console.WriteLine($"Комплексы в которые можно поселить {animal}");
             bool flag1 = false;
-            int[] mas = new  int[complices.Count];
+            int[] mas = new int[complices.Count];
             int num;
             for (int i = 0; i < complices.Count; i++)
             {
@@ -270,7 +287,7 @@ namespace Coursework
                     var key = complices[i].presents.ElementAt(j).Key;
                     if (key.GetName == animal)
                     {
-                        Console.WriteLine($"{"№"+i + " " + complices[i].ToString()}");
+                        Console.WriteLine($"{"№" + i + " " + complices[i].ToString()}");
                         mas[i] = i;
                         flag1 = true;
                     }
@@ -299,7 +316,7 @@ namespace Coursework
                 else Console.WriteLine("Ошибка ввода");
             }
             complices[num].AddAni(animal, kol_vo, ref flag);
-            
+
 
         }
 
@@ -314,9 +331,9 @@ namespace Coursework
             int s = 1;
             foreach (var x in complices)
             {
-                Console.WriteLine($"{"№"+s+" "+x.ToString()}");
+                Console.WriteLine($"{"№" + s + " " + x.ToString()}");
                 s++;
-            }  
+            }
             Console.WriteLine("Введите номер комплекса из которого вы хотите удалить животное");
             int nc = Int32.Parse(Console.ReadLine());
             nc--;
@@ -360,17 +377,17 @@ namespace Coursework
             Console.WriteLine("Комплексы:");
             for (int i = 0; i < newcom.Count; i++)
             {
-                Console.WriteLine($"№{i+1} {newcom[i].ToString()}");
+                Console.WriteLine($"№{i + 1} {newcom[i].ToString()}");
             }
             for (; ; )
             {
                 Console.WriteLine("Введите номер комплекса");
-                nc = Int32.Parse(Console.ReadLine());    
-                if (nc-1 >= 0 &&  nc-1 < newcom.Count) break;
+                nc = Int32.Parse(Console.ReadLine());
+                if (nc - 1 >= 0 && nc - 1 < newcom.Count) break;
                 else Console.WriteLine("Ошибка ввода");
             }
-                newcom[--nc].adding(animals);
-            
+            newcom[--nc].adding(animals);
+
         }
 
 
---
+++ main/Curs/SaveManeger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Curs
+{
+    interface IWritableObject
+    {
+        void Write(SaveManeger man);
+    }
+    public class SaveManeger
+    {
+        FileInfo file;
+        public void SaveManager(string filename)
+        {
+            file = new FileInfo(filename);
+            file.CreateText();
+        }
+
+        public void WriteLine(string line)
+        {
+            StreamWriter output = file.AppendText();
+            output.WriteLine(line);
+            output.Close();
+        }
+        void WriteObject(IWritableObject obj)
+        {
+            obj.Write(this);
+        }
+    }
+}
